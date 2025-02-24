497bea9217651f4915c97b26ee50372451645005
fearanha <felipearanha.c@gmail.com>
2025-02-24 15:26:34 -0300

    github test desktop

--- apps/web/src/app/(app)/org/[slug]/project/[project]/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/project/[project]/page.tsx
@@ -1,7 +1,7 @@
 export default async function Project() {
   return (
     <div className="space-y-4 p-4">
-      <h1 className="text-2xl font-bold">Project</h1>
+      <h1 className="text-2xl font-bold">Project dev...</h1>
     </div>
   )
 }
